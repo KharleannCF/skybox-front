aca7ab8abf3e32357bdf15656ceef77b5c271ba4
kharleanncf <kharleann@gmail.com>
2021-06-17 16:42:58 -0400

    loading

--- src/views/index/Index.jsx
+++ src/views/index/Index.jsx
@@ -2,15 +2,18 @@ import React, {useState, useEffect} from "react"
 import {Form} from "react-bootstrap"
 import {handleSubmit, getScenes} from "./logic/formLogic"
 import {Link} from "react-router-dom"
+import {Progress} from "reactstrap"
 function Index(){
     const [scenes, setScenes] = useState([])
+    const [loaded, setLoaded] = useState(0)
+    const [loading, setLoading] = useState(false)
     useEffect(async ()=>{
         getScenes(setScenes)
     },[])
     return(
         <div>
             <h1>Upload 6 images</h1>
-            <Form onSubmit={(e)=>{handleSubmit(e)}}>
+            <Form onSubmit={(e)=>{handleSubmit(e, setLoaded, setLoading)}}>
                 <Form.Group>
                 <Form.Control type="text" id="text" placeholder="Enter name of the project" />
                     <Form.File id="front" label="Frente" />
@@ -20,7 +23,10 @@ function Index(){
                     <Form.File id="top" label="arriba" />
                     <Form.File id="bottom" label="abajo" />
                 </Form.Group>
-                <button>Send</button>
+                <div class="form-group">
+                    <Progress max="100" color="success" value={loaded}>{Math.round(loaded,2)}%</Progress>
+                </div>
+                <button disabled={loading}>Send</button>
             </Form>
             <div>
                 <ul>
